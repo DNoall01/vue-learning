92dbd837a6e5828a6a7c5956c88d9b20a90b9848
Dylan Noall <dylannoall@outlook.com>
2025-04-20 16:06:17 -0500

    vue-router-basic-routing

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,19 @@
 <template>
-  <h1>Message: {{ message }}</h1>
-  <hr>
-  <h1>Number: {{ number }}</h1>
-  <hr>
-  <h1>doubleNum(50): {{ doubleNum(50) }}</h1>
-  <hr>
-  <h1>number * 2 = {{ number * 2 }}</h1>
-  <hr>
-  <h1>{{ number > 150 ? 'number is greater than 150' : 'number is less than 150' }}</h1>
-  <h1 v-text="number"></h1>
-  <hr />
-  <h1>harry: {{ harry }}</h1>
-  <h1>harry.name: {{ harry.name }}</h1>
-  <h1>hogwartsWizards: {{ hogwartsWizards }}</h1>
-  <h1>hogwartsWizards[0]: {{ hogwartsWizards[0] }}</h1>
-  <hr />
-
-  <h1>rawHtml: {{ rawHtml }}</h1>
-  <h1 v-text="rawHtml"></h1>
-  <h1 v-html="rawHtml"></h1>
+  <div class="app">
+    <Navigation></Navigation>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup>
-  let message = 'Hello, Vue!';
-  let number = 50;
+import Navigation from '@/components/Navigation.vue';
 
-  function doubleNum(num){
-    return num * 2;
-  }
-
-  let harry = {
-  id: 1001,
-  name: 'Harry Potter',
-  house: 'Gryffindor',
-  age: 17, // Age during the final battle of Hogwarts
-  wand: {
-    core: 'Phoenix feather',
-    wood: 'Holly'
-  }
-}
-const hogwartsWizards = [
-  {
-    id: 1001,
-    name: 'Harry Potter',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Phoenix feather',
-      wood: 'Holly'
-    }
-  },
-  {
-    id: 1002,
-    name: 'Hermione Granger',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Vine'
-    }
-  },
-  {
-    id: 1003,
-    name: 'Ron Weasley',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Unicorn hair',
-      wood: 'Willow'
-    }
-  },
-  {
-    id: 1004,
-    name: 'Draco Malfoy',
-    house: 'Slytherin',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Hawthorn'
-    }
-  }
-]
-
-let rawHtml = '<span style="color: red">This should be red.</span>'
 </script>
 
 <style lang="scss" scoped>
-
+.app {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
 </style>
